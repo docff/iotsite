b935a9f7dfca6bd2729a8d03c63fa2e095cca749
George Xypolitos <gxypol@gmail.com>
2018-11-24 16:12:59 +0200

    Init Commit

---
+++ src/main/java/com/project/iotsite/IoTSiteApplication.java
@@ -0,0 +1,12 @@
+package com.project.iotsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoTSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IoTSiteApplication.class, args);
+    }
+}
